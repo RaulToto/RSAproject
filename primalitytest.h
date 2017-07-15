b16c2ae2817bc5004d1fdfd07f773d6c9c3fd90a
RaulToto <djbrexman@gmail.com>
2017-07-15 08:58:07 -0500

    updata class primality

--- primalitytest.h
+++ primalitytest.h
@@ -6,6 +6,11 @@ class PrimalityTest
 {
 public:
     PrimalityTest();
+    ZZ module(ZZ,ZZ);
+    ZZ power(ZZ , ZZ ,ZZ);
+    bool miillerTest(ZZ,ZZ);
+    bool isPrime(ZZ,ZZ);
+    ~PrimalityTest();
 };
 
 #endif // PRIMALITYTEST_H
